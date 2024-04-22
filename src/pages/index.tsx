02c3db3fb29635d365acbc18f3d7e572da5d198f
syafikadin <syafik.adin222@gmail.com>
2024-04-22 22:29:24 +0700

    Fix: fetch data from API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,51 @@
 import * as React from "react";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Table from "@/components/Table/Index";
+import { Auth } from "@/components/Auth/Auth";
+import { useQuery } from "@tanstack/react-query";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { useEffect, useState } from "react";
+
+const queryClient = new QueryClient()
 
 export default function Home() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        setIsLoggedIn(true)
+    }
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {!isLoggedIn ? (    //handle is logged in or not
+                <Auth onLogin={handleLogin} />
+            ) : (
+                <App />         //Go to app when logged in
+            )}
+        </QueryClientProvider>
+    );
+}
+
+function App() {
+
+    const [accessToken, setAccessToken] = useState(localStorage.accessToken) //retrive token
+
+    const { isLoading, error, data, isSuccess } = useQuery({
+        queryKey: ['repoData'],
+        queryFn: () =>
+            fetch('https://datacore-dev.machinevision.global/items/perusahaan_bei', {
+                headers: {
+                    Authorization: `Bearer ${accessToken}` //Use Bearer Token Authorization
+                }
+            }).then((res) =>
+                res.json(),
+            ),
+    })
+
+    if (isLoading) return 'Loading...'
+
+    if (error) return 'An error has occurred: ' + error.message
+
     return (
         <Box
             sx={{
@@ -16,9 +57,28 @@ export default function Home() {
                 alignItems: "center",
             }}
         >
-            <Typography variant="h4" component="h1" sx={{mb: 2}}>
-                Hello world
-            </Typography>
+
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Kode Saham</th>
+                        <th>Nama Perusahaan</th>
+                        <th>Tanggal Listing</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.data.map((item: any) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.kode_saham}</td>
+                            <td>{item.nama}</td>
+                            <td>{item.tanggal_listing}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
         </Box>
-    );
+    )
 }
